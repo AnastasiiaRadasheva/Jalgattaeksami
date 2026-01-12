033e369e140318f628514591607cfe2315d0e960
Anastasiia Radasheva <eha20082@gmail.com>
2026-01-12 12:58:53 +0200

    lisa fail config

---
+++ konf.php
@@ -0,0 +1,6 @@
+<?php
+ $baasiaadress="localhost";
+ $baasikasutaja="jalgrattaeksami";
+ $baasiparool="12345";
+ $baasinimi="jalgrattaeksami";
+ $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi); 
