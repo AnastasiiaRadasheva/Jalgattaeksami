d5bf1e4aa140e98c81532067ca0b5c335fc42bc6
Anastasiia Radasheva <eha20082@gmail.com>
2026-01-12 13:05:05 +0200

    lehel Kasutaja registreerimine on lisatud

--- konf.php
+++ konf.php
@@ -1,6 +1,6 @@
 <?php
  $baasiaadress="localhost";
- $baasikasutaja="jalgrattaeksami";
+ $baasikasutaja="jalgrattaeksam";
  $baasiparool="12345";
- $baasinimi="jalgrattaeksami";
- $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi); 
+ $baasinimi="jalgrattaeksam";
+ $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
